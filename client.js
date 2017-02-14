98fcfb4966a0dad8bce6215cf0f4de15ff2e9abc
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-13 21:23:47 -0600

    Updated JS table data cells

--- client.js
+++ client.js
@@ -1,23 +1,27 @@
 $(document).ready(function(){
-
-
   $('#submitButton').on('click', function(){
     var firstName = $('#firstName').val();
     var lastName = $('#lastName').val();
     var idNumber = $('#idNumber').val();
     var jobTitle = $('#jobTitle').val();
     var annualSalary = $('#annualSalary').val();
-    var employeeForm = firstName + ' ' + lastName + ' ' + idNumber + ' ' + jobTitle + ' ' + annualSalary;
-    $('#nameContainer').append('<p>' + employeeForm + " " +
-    '<button class="deleteButton">Delete</button></p>');
-  });
-  $('#submitButton').on('click', function() {
+
+    $('employeeTable').append('<tr>'
+      '<td>' + firstName + '</td>'
+      '<td>' + lastName + '</td>'
+      '<td>'+ idNumber + '</td>'
+      '<td>' + jobTitle + '</td>'
+      '<td>' + annualSalary + '</td>'
+      '<td>< button class="deleteButton" data-salary"' + annualSalary + '">Delete ' + firstName + '</button></td' + '</tr>');
+
+
     var monthlySalaryExpenditures = parseInt($('#annualSalary').val()) / 12;
     $('#monthlySalaryCost').append(monthlySalaryExpenditures);
-  });
-  $('#submitButton').on('click', function(){
+
+
     $('input').val('')
   });
+
   $('#nameContainer').on('click', '.deleteButton', function(){
     $(this).parent().remove();
   });
